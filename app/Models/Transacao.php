cd74770042d9eeb78771678f8039cf60a7f50006
Renan Couri <renancouri123@gmail.com>
2024-08-13 01:34:59 -0300

    Alterações no vetor 'fillable' das models User e Transacao.

--- app/Models/Transacao.php
+++ app/Models/Transacao.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Transacao extends Model
 {
     use HasFactory;
+    protected $table = 'transacoes'; //Coloco aqui devido ao plural irregular da palavra 'transação'.
+    protected $fillable = ['valor','tipo'];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'foto',
+        'numero_telefone',
+        'CPF',
+        'data_nascimento',
+        'cargo'
     ];
 
     /**
